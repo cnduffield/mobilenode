84b8565241fb65cf07d5220adbc598ba75c726f4
cnduffield <cnduffield@hotmail.com>
2018-09-07 22:35:20 -0400

    updated

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-document.write('Hello HTML');
+app.get('/', function(req, res){
+    //res.sendfile('default.html', { root: __dirname + "/relative_path_of_file" } );
+	res.sendfile('default.html', { root: __dirname } );
+});
