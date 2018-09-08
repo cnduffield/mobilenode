902b59a50ac723e1b571861d397531a5cd68a359
cnduffield <cnduffield@hotmail.com>
2018-09-08 14:28:04 -0400

    add http

--- index.js
+++ index.js
@@ -4,11 +4,29 @@
 });*/
 var path = require('path');
 var express = require('express');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+
 var app = express();
 var port = 8080;
 var htmlPath = path.join(__dirname, 'images');
 
 app.use(express.static(htmlPath));
+
+http.createServer(function (req, res) {
+	  var q = url.parse(req.url, true);
+	  var filename = "." + q.pathname;
+	  fs.readFile(filename, function(err, data) {
+	    if (err) {
+	      res.writeHead(404, {'Content-Type': 'text/html'});
+	      return res.end("404 Not Found");
+	    }  
+	    res.writeHead(200, {'Content-Type': 'text/html'});
+	    res.write(data);
+	    return res.end();
+	  });
 
 app.get('/', function (req, res, next) {
  res.sendFile( __dirname + '/index.html');
