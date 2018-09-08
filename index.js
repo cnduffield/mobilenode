4a4749b8f6f20ae7188fe93ed3d44855ee9304e1
cnduffield <cnduffield@hotmail.com>
2018-09-08 14:52:28 -0400

    link

--- index.js
+++ index.js
@@ -23,12 +23,9 @@ http.createServer(function (req, res) {
 	  var filename = "." + q.pathname;
 	  fs.readFile(filename, function(err, data) {
 	    if (err) {
-	      //res.writeHead(404, {'Content-Type': 'text/html'});
-	      //return res.end("404 Not Found");
-	    	res.writeHead(200, {'Content-Type': 'text/html'});
-	    	res.sendFile( __dirname + '/index.html');
-	 	    res.write(data);
-	 	    return res.end();
+	      res.writeHead(404, {'Content-Type': 'text/html'});
+	      return res.end("<a href='index.html'>Click here to start</a>");
+	    	
 	    }  
 	    res.writeHead(200, {'Content-Type': 'text/html'});
 	    res.write(data);
