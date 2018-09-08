cd50773542adf67e964972f24b432587a63d1bee
cnduffield <cnduffield@hotmail.com>
2018-09-08 13:34:08 -0400

    updates

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
     //res.sendfile('default.html', { root: __dirname + "/relative_path_of_file" } );
 	res.sendfile('default.html', { root: __dirname } );
 });*/
-
+var path = require('path');
 var express = require('express');
 var app = express();
 var port = 8080;
+var htmlPath = path.join(__dirname, 'html');
+
+app.use(express.static(htmlPath));
 
 app.get('/', function (req, res, next) {
  res.sendFile( __dirname + '/index.html');
