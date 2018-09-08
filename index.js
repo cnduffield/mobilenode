b6174c373f5accd2b9e3f359f49c38d7fe7602ff
cnduffield <cnduffield@hotmail.com>
2018-09-08 14:32:57 -0400

    updated port

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ var htmlPath = path.join(__dirname, 'images');
 
 app.use(express.static(htmlPath));
 
+http.createServer(function (req, res) {
+	  var q = url.parse(req.url, true);
+	  var filename = "." + q.pathname;
+	  fs.readFile(filename, function(err, data) {
+	    if (err) {
+	      res.writeHead(404, {'Content-Type': 'text/html'});
+	      return res.end("404 Not Found");
+	    }  
+	    res.writeHead(200, {'Content-Type': 'text/html'});
+	    res.write(data);
+	    return res.end();
+	  });
+	}).listen(port);
 
 
 app.get('/', function (req, res, next) {
