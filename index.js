a738aecb15c32d915645ec3513d91c27096afe77
cnduffield <cnduffield@hotmail.com>
2018-09-08 11:36:03 -0400

    test

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-app.get('/', function(req, res){
+/*app.get('/', function(req, res){
     //res.sendfile('default.html', { root: __dirname + "/relative_path_of_file" } );
 	res.sendfile('default.html', { root: __dirname } );
+});*/
+
+var express = require('express');
+var app = express();
+var port = 8080;
+
+app.get('/', function (req, res, next) {
+ res.sendFile( __dirname + '/index.html');
+});
+
+app.listen(port, '0.0.0.0', function() {
+ console.log('Server running at port ' + port);
 });
