96be4187e8904a77de901f16d26141f5ba84548f
cnduffield <cnduffield@hotmail.com>
2018-09-19 12:05:15 -0500

    env change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var express = require('express');
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var envTypeColor = process.env.COLOR;
+var envTypeColor = process.env.color;
 
 var app = express();
 var port = 8080;
